b24f02df967845c5f414dedd71a148e5433f3f3a
Joe Thorngren <joe.thorngren@tackmobile.com>
2018-01-14 21:36:49 -0600

    Handjam, no extending DaggerApplication

--- app/src/main/java/com/lighthouselabs/dagger2talk/Dagger2Application.kt
+++ app/src/main/java/com/lighthouselabs/dagger2talk/Dagger2Application.kt
@@ -1,5 +1,29 @@
 package com.lighthouselabs.dagger2talk
 
+import android.app.Activity
 import android.app.Application
+import android.widget.Toast
+import com.lighthouselabs.dagger2talk.dagger.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class Dagger2Application : Application() 
+class Dagger2Application : Application(), HasActivityInjector {
+
+    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
+    @Inject lateinit var fooString: String
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DaggerApplicationComponent.create().inject(this)
+
+        Toast.makeText(this, fooString, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return dispatchingActivityInjector
+    }
+
+}
