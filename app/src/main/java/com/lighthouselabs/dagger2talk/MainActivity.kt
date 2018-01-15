82169638c343481ec61de145335d012d81359d89
Joe Thorngren <joe.thorngren@tackmobile.com>
2018-01-14 22:12:49 -0600

    @ContributesAndroidInjector w/AppCompatActivity.

--- app/src/main/java/com/lighthouselabs/dagger2talk/MainActivity.kt
+++ app/src/main/java/com/lighthouselabs/dagger2talk/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.lighthouselabs.dagger2talk
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import dagger.android.DaggerActivity
+import dagger.android.AndroidInjection
 import javax.inject.Inject
 
-class MainActivity : DaggerActivity() {
+class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var fooString: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
